799e5c7b613224a323a94db619e7c5e29bced03b
鸿则 <hungtcs@163.com>
2018-05-06 21:38:14 +0800

    修复潜在问题

--- server/devices-checker.js
+++ server/devices-checker.js
@@ -14,10 +14,11 @@ module.exports = {
           url: `http://${ device.ipAddress }/status`,
           timeout: 10000,
         }).then(response => JSON.parse(response)).then(({ status }) => {
-          logger.info(`active device ${ device.ipAddress }, mac address: ${ key }`);
           if(status !== 0) {
             throw new Error(`bad status code ${ status }`);
           }
+          logger.info(`active device ${ device.ipAddress }, mac address: ${ key }`);
+          device.inactiveTime = 0;
         }).catch(err => {
           logger.warn(err.message);
           logger.info(`found inactive device ${ device.ipAddress }, mac address: ${ key }`);
